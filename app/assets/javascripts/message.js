5fd741eec591bdbea196c66bbca3607ee2e6210c
yuichiro-cloud <ghjhbc1@gmail.com>
2020-01-11 12:16:07 +0900

    first-commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,5 @@
 $(function(){ 
-  function buildHTML(message){
+  var buildHTML = function buildHTML(message){
    if ( message.image ) {
      var html =
   `<div class="main-chat__message-list__message" data-message-id=${message.id}>
@@ -40,7 +40,7 @@ $(function(){
 $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
- var url = $(this).attr('action')
+ var url = $(this).attr('action');
  $.ajax({
    url: url,
    type: "POST",
@@ -49,8 +49,9 @@ $('#new_message').on('submit', function(e){
    processData: false,
    contentType: false
   })
-  .done(function(data){
-    var html = buildHTML(data);
+  .done(function(formData){
+    console.log("OK");
+    var html = buildHTML(formData);
     $('.main-chat__message-list').append(html);   
     $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});   
     $('form')[0].reset();
@@ -59,6 +60,33 @@ $('#new_message').on('submit', function(e){
     })
   .fail(function() {
     alert("メッセージ送信に失敗しました");
-    });
+    })
   })
+  var reloadMessages = function() {
+    last_message_id = $('.main-chat__message-list__message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+       var insertHTML = '';
+       $.each(messages, function(i, message) {
+         insertHTML += buildHTML(message)
+       });
+       $('.main-chat__message-list').append(insertHTML);
+       $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".main-chat__message-form-padding__form__send").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 4000);
+  }
 });
